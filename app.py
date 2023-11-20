b020100845ea7b3a6b0e97a91a6b5a08a95c7825
madhavphad777 <122075368+madhavphad777@users.noreply.github.com>
2023-11-20 10:11:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 from sklearn.impute import SimpleImputer
 from sklearn.model_selection import train_test_split
